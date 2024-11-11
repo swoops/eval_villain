0ff8fff3522bdfe9fd5ef7b9b1bd096dfefdf8bc
Dennis Goodlett <dennis@doyensec.com>
2024-11-11 15:41:34 -0500

    Update options page to include sponsor

--- src/pages/options/options.js
+++
@@ -1,9 +0,0 @@
-function listener(ev) {
-	let id = ev.target.id;
-	if (id == "config" ) {
-		let confUrl = browser.runtime.getURL("/pages/config/config.html");
-		browser.tabs.create({url:confUrl});
-		return;
-	}
-}
-document.addEventListener("click", listener);
